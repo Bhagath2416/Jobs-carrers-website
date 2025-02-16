ca7fd694e6c807fe921b4fb9dc2cad5c81342907
Bhagath2416 <150446973+Bhagath2416@users.noreply.github.com>
2025-02-16 13:58:28 +0000

    added the flask project

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app=Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "Hello, jovian"
+  return "Hello, bhagathkumar"
 if __name__=="__main__" :
- app.run(host='0.0.0.0', port=3000, debug=True)
+ app.run(host='0.0.0.0', debug=True)
